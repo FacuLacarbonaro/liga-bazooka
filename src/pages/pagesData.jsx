1954b95fe5b795f021b85eb1b7d1017fed532e80
facundolacarbonaro <faculacarbonaro@gmail.com>
2024-10-06 18:08:23 +1300

    first setup

---
+++ src/pages/pagesData.jsx
@@ -0,0 +1,15 @@
+import About from "./About";
+import Home from "./Home";
+
+export const pagesData = [
+  {
+    path: "",
+    element: <Home />,
+    title: "home",
+  },
+  {
+    path: "/about",
+    element: <About />,
+    title: "home",
+  },
+];
